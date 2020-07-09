011dca1b7923feb1832acbbae77c8453bce16bba
mehdi-monfort <mehdimonfort@gmail.com>
2020-07-09 15:37:15 +0200

    add c_id in Categories and Relations tables

--- api/Database/table.py
+++ api/Database/table.py
@@ -1,4 +1,3 @@
-"""program constant, """
 """sql request to add products and tables necessary for use"""
 
 ##############################################################################
@@ -8,37 +7,38 @@ TABLES = {}
 
 TABLES["Categories"] = (
     "CREATE TABLE `Categories` ("
-    "  categorie VARCHAR(210) NOT NULL PRIMARY KEY"
+    "  cat_id SMALLINT AUTO_INCREMENT PRIMARY KEY,"
+    "  categorie VARCHAR(210) NOT NULL"
     ") ENGINE=InnoDB"
 )
 
 TABLES["Products"] = (
     "CREATE TABLE `Products` ("
+    "  score CHAR(1) NOT NULL,"
     "  barcode VARCHAR(14) NOT NULL PRIMARY KEY,"
     "  name VARCHAR(210) NOT NULL,"
-    "  score CHAR(1) NULL,"
     "  url TEXT NULL,"
     "  market VARCHAR(210) NULL"
     ") ENGINE=InnoDB"
 )
 
-TABLES["Favorites"] = (
-    "CREATE TABLE `Favorites` ("
-    "  fav_id VARCHAR(14) PRIMARY KEY, "
-    "  CONSTRAINT fav_id FOREIGN KEY (fav_id) "
-    "     REFERENCES Products (barcode) ON DELETE CASCADE,"
-    "  UNIQUE KEY (fav_id)"
+TABLES["Relations"] = (
+    "CREATE TABLE `Relations` ("
+    "  cat_id SMALLINT NOT NULL, "
+    "  barcode VARCHAR(14) NOT NULL,"
+    "  CONSTRAINT cat_id FOREIGN KEY (cat_id) "
+    "     REFERENCES Categories (cat_id) ON DELETE CASCADE,"
+    "  CONSTRAINT barcode FOREIGN KEY (barcode) "
+    "     REFERENCES Products (barcode) ON DELETE CASCADE"
     ") ENGINE=InnoDB"
 )
 
-TABLES["Relations"] = (
-    "CREATE TABLE `Relations` ("
-    "  categorie VARCHAR(210) NOT NULL, "
-    "  barcode VARCHAR(14) NOT NULL,"
-    "  CONSTRAINT categorie FOREIGN KEY (categorie) "
-    "     REFERENCES Categories (categorie) ON DELETE CASCADE,"
-    "  CONSTRAINT barcode FOREIGN KEY (barcode) "
-    "     REFERENCES Products (barcode) ON DELETE CASCADE"
+TABLES["Favorites"] = (
+    "CREATE TABLE `Favorites` ("
+    "  code VARCHAR(14) PRIMARY KEY, "
+    "  CONSTRAINT code FOREIGN KEY (code) "
+    "     REFERENCES Relations (barcode) ON DELETE CASCADE,"
+    "  UNIQUE KEY (code)"
     ") ENGINE=InnoDB"
 )
 
