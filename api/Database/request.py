a7f216d374adda108f8dbcdf7258533b9c6a4072
mehdi-monfort <mehdimonfort@gmail.com>
2020-07-10 08:55:12 +0200

    creation of a file for requests

---
+++ api/Database/request.py
@@ -0,0 +1,65 @@
+import requests
+from Class.category import Category
+from Class.product import Product
+from Class.relation import Relation
+
+
+class Request():
+
+   def create_database(self, database):
+        url = "https://world.openfoodfacts.org/cgi/search.pl?search_terms={}&search_simple=1&page_size=500&action=process&json=1"
+        categories = [
+            "Produits fermentés",
+            "Jus et nectars",
+            "Gelées de fruits",
+            "Matières grasses",
+            "snacks",
+        ]
+        print("Waiting, request in progress ...")
+        for cat_id, name in enumerate(categories):
+            n_prod = 0
+            n_prod_remove = 0
+            n_prod_keep = 0
+            cat_id += 1
+            category = Category(cat_id, name)
+            database.add_category(category.name)
+            print("------------------------------------------------------")
+            print(category.name, "...waiting...")
+            print("------------------------------------------------------")
+            response = requests.get(url.format(name))
+            resp = response.json()
+            for i in range(500):
+                try:
+                    prod = Product(
+                        resp["products"][i].get("nutrition_grades", "0"),
+                        resp["products"][i].get("_id", 0),
+                        resp["products"][i].get("product_name_fr", "0"),
+                        resp["products"][i].get("url", "absent"),
+                        resp["products"][i].get("stores", "absent"),
+                        )
+                    n_prod += 1
+                    checkers = [
+                        3 * 10 ** 12 < int(prod.barcode) < 8 * 10 ** 12,
+                        str(prod.name) != "0" and str(prod.name) != "",
+                        str(prod.nutriscore) != "0"
+                    ]
+                    if all(checkers):
+                        database.add_product(
+                            (prod.nutriscore).upper(),
+                            prod.barcode,
+                            prod.name,
+                            prod.url,
+                            prod.market,
+                        )
+                        n_prod_keep += 1
+                        link = Relation(category.cat_id, prod.barcode)
+                        database.add_relation(link.cat_id, link.barcode)
+                    else:
+                        continue
+                except IndexError:
+                    continue
+                n_prod_ignore = n_prod - n_prod_keep
+            print(f"{n_prod} products collected.")
+            print(f"{n_prod_ignore} products ignored.")
+            print(f"{n_prod_keep} products added.")
+            print("Request complete.")
