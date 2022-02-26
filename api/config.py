400435d28e7ce70474ef7f1b39ebc975fb4c8d70
mehdi-monfort <mehdimonfort@gmail.com>
2022-02-26 20:51:47 +0100

    error: pull request restoration to original state

--- api/config.py
+++ api/config.py
@@ -5,6 +5,7 @@ DB_CONFIG = {
     "user": "p5_user",
     "password": "p5_user",
     "database": "project5",
+    "auth_plugin": "mysql_native_password",
     "charset": "utf8",
     "autocommit": True,
 }
